adf5fd8d1e874a3ba7b036c16912b373f6c107a8
_Pear <63015754+CobyPear@users.noreply.github.com>
2020-07-22 20:21:52 -0500

    added npm dotenv, refactored config.json to config.js

---
+++ config/config.js
@@ -0,0 +1,24 @@
+require('dotenv').config
+module.exports = {
+  "development": {
+    "username": process.env.USER_LOCAL,
+    "password": process.env.PASS_LOCAL,
+    "database": process.env.NAME_LOCAL,
+    "host": process.env.HOST_LOCAL,
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
