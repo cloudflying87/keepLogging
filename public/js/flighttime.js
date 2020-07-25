317c722ef7850cd63d401b51f82c22d16c4138be
_Pear <63015754+CobyPear@users.noreply.github.com>
2020-07-25 18:13:06 -0500

    writeAircraft() working, event listener on add aircraft working

--- public/js/flighttime.js
+++ public/js/flighttime.js
@@ -23,20 +23,13 @@ $("#create-flight").on("click", function (e) {
     createFlight();
 });
 
-$('#createFlightButton').on('click', function (e) {
-    e.preventDefault();
-    writeFlightTime();
-})
 
-$('#create-aircraft').on('click', function(e) {
+$('#create-aircraft').on('click', function (e) {
     e.preventDefault();
     $accordian.empty();
     createAircraft();
 })
 
-$('#create-aircraft-button').on('click', function (e) {
-    e.preventDefault();
-})
 
 function createInputLoop(arr1, arr2) {
     $accordian.append('<p>', arr2[0])
@@ -46,10 +39,24 @@ function createInputLoop(arr1, arr2) {
         $input.attr('id', arr1[i]);
         $input.attr('placeholder', arr1[0]);
         $input.addClass(arr2[1]);
-        $label.text(arr2[i+1]);
-        $accordian.append($label, $input)
+        $label.text(arr2[i + 1]);
+        $accordian.append($label, $input);
     };
-}
+};
+
+function createInputLoopCheckboxes(arr1, arr2) {
+    $accordian.append('<p>', arr2[0])
+    for (let i = 1; i < arr1.length; i++) {
+        const $input = $('<input type=check-box>');
+        const $label = $("<label>");
+        $input.attr('id', arr1[i]);
+        $input.addClass(arr2[1]);
+        $label.text(arr2[i + 1]);
+        // $div.addClass("input-group-prepend");
+        // $div.append($input)
+        $accordian.append($label, $input);
+    };
+};
 // Used to make all of the input boxes for the create flight section
 function createFlight() {
     //General Flight Info
@@ -59,98 +66,98 @@ function createFlight() {
 
     // Landings and Approaches
     const approachLanding = [0, 'approach', 'holds', 'totalLandings', 'dayLdg', 'nightLdg']
-    const approachLandingInfo = ['Aproaches and Landings','app', 'Approach', 'Holds', 'Total Landings', 'Day Landing', 'Night Landing']
+    const approachLandingInfo = ['Aproaches and Landings', 'app', 'Approach', 'Holds', 'Total Landings', 'Day Landing', 'Night Landing']
     createInputLoop(approachLanding, approachLandingInfo)
     // Times
     const flightTimesIds = [0.00, 'total', 'cxt', 'night', 'hood', 'imc', 'dual', 'cfi', 'sic', 'pic']
-    const flightTimesidsLabel = ['Flight Times','times', 'Total', 'Cross-Country', 'Night', 'Simulated Instrument', 'IMC', 'Dual (Instruction Recieved)', 'CFI (Instructor)', 'SIC (Second in Command)', 'PIC (Pilot in Command)']
+    const flightTimesidsLabel = ['Flight Times', 'times', 'Total', 'Cross-Country', 'Night', 'Simulated Instrument', 'IMC', 'Dual (Instruction Recieved)', 'CFI (Instructor)', 'SIC (Second in Command)', 'PIC (Pilot in Command)']
     createInputLoop(flightTimesIds, flightTimesidsLabel)
 
     const newButton = $("<button>").attr('id', 'createFlightButton').text("Add Flight")
     $accordian.append(newButton)
 
-    $("#createFlightButton").click(function (event){
+    $("#createFlightButton").click(function (event) {
         event.preventDefault();
         writeFlightTime();
     })
 };
 
-function writeFlightTime(){
+function writeFlightTime() {
     const NULL = null
-   $('.form-control').each(function(){
+    $('.form-control').each(function () {
         console.log($(this).attr('class'))
-    if ($(this).hasClass('general')) {
-        const generalName = $(this).attr('id')
-    }
-            Error();
+        if ($(this).hasClass('general')) {
+            const generalName = $(this).attr('id')
+        }
+        Error();
     });
 
-    if ($("#imc").val().trim()==''){
+    if ($("#imc").val().trim() == '') {
         imc = 0.00
     } else {
         imc = $("#imc").val().trim()
     };
-    if ($("#hood").val().trim()==''){
+    if ($("#hood").val().trim() == '') {
         hood = 0.00
     } else {
         hood = $("#hood").val().trim()
     };
-    if ($("#holds").val().trim()==''){
+    if ($("#holds").val().trim() == '') {
         holds = 0
     } else {
         holds = $("#holds").val().trim()
     };
-    if ($("#pic").val().trim()==''){
+    if ($("#pic").val().trim() == '') {
         pic = NULL
     } else {
         pic = $("#pic").val().trim()
     };
-    if ($("#sic").val().trim()==''){
+    if ($("#sic").val().trim() == '') {
         sic = 0.00
     } else {
         sic = $("#sic").val().trim()
     };
-    if ($("#dual").val().trim()==''){
+    if ($("#dual").val().trim() == '') {
         dual = 0.00
     } else {
         dual = $("#dual").val().trim()
     };
-    if ($("#cfi").val().trim()==''){
+    if ($("#cfi").val().trim() == '') {
         cfi = 0.00
     } else {
         cfi = $("#cfi").val().trim()
     };
-    if ($("#cxt").val().trim()==''){
+    if ($("#cxt").val().trim() == '') {
         cxt = 0.00
     } else {
         cxt = $("#cxt").val().trim()
     };
-    if ($("#night").val().trim()==''){
+    if ($("#night").val().trim() == '') {
         night = 0.00
     } else {
         night = $("#night").val().trim()
     };
-    if ($("#total").val().trim()==''){
+    if ($("#total").val().trim() == '') {
         total = 0.00
     } else {
         total = $("#total").val().trim()
     };
-    if ($("#approach").val().trim()==''){
+    if ($("#approach").val().trim() == '') {
         approach = 0.00
     } else {
         approach = $("#approach").val().trim()
     };
-    if ($("#totalLandings").val().trim()==''){
+    if ($("#totalLandings").val().trim() == '') {
         totalLandings = 0.00
     } else {
         totalLandings = $("#totalLandings").val().trim()
     };
-    if ($("#dayLdg").val().trim()==''){
+    if ($("#dayLdg").val().trim() == '') {
         dayLdg = 0.00
     } else {
         dayLdg = $("#dayLdg").val().trim()
     };
-    if ($("#nightLdg").val().trim()==''){
+    if ($("#nightLdg").val().trim() == '') {
         nightLdg = 0.00
     } else {
         nightLdg = $("#nightLdg").val().trim()
@@ -181,27 +188,68 @@ function writeFlightTime(){
         pic: pic,
     })
 
-    .then(function() { result =>
-        console.log(result)
-        // If there's an error, log the error
-      })
-      .catch(function(err) {
-        console.log(err.responseJSON.parent)
-      });
+        .then(function () {
+            result =>
+                console.log(result)
+            // If there's an error, log the error
+        })
+        .catch(function (err) {
+            console.log(err.responseJSON.parent)
+        });
 
 }
 
 // function for input boxes for create aircraft section
 function createAircraft() {
 
-    const generalAircraft = ['', 'aircraftType', 'class', 'numEngine', 'tailWheel', 'complex', 'highPerf', 'turboFan', 'turboProp', 'rototcraft', 'poweredLift'];
+    const generalAircraft = ['', 'aircraftType', 'class', 'numEngine'];
 
-    const generalAircraftInfo = ['General Aircraft Information', 'general', 'Aircraft Type', 'Class', 'Number of Engines', 'Tail Wheel', 'Complex', 'High Perf', 'Turbo Fan', 'Turbo Prop', 'Rotocraft', 'Powered Lift'];
+    const generalAircraftLabels = ['General Aircraft Information', 'general', 'Aircraft Type', 'Class', 'Number of Engines'];
 
-    createInputLoop(generalAircraft, generalAircraftInfo);
+    createInputLoop(generalAircraft, generalAircraftLabels);
+
+    const generalAircraftBoolean = ['', 'tailWheel', 'complex', 'highPerf', 'turboFan', 'turboProp', 'rototcraft', 'poweredLift'];
+
+    const generalAircraftBooleanLabels = ['', 'general', 'Tail Wheel', 'Complex', 'High Perf', 'Turbo Fan', 'Turbo Prop', 'Rotocraft', 'Powered Lift'];
+
+    createInputLoopCheckboxes(generalAircraftBoolean, generalAircraftBooleanLabels);
 
     const newButton = $("<button>").attr('id', 'create-aircraft-button').text("Add Aircraft")
     $accordian.append(newButton)
+
+    $('#create-aircraft-button').on('click', function (e) {
+        e.preventDefault();
+        console.log("working");
+        writeAircraft();
+    })
+};
+
+// function for writing create aircraft fields to the db
+function writeAircraft() {
+    $('.form-control').each(function () {
+        console.log($(this).attr('class'))
+        if ($(this).hasClass('general')) {
+            const generalName = $(this).attr('id')
+        } else {
+            Error();
+        };
+    });
+
+    $.post("/api/aircraft", {
+        userId: userData.id,
+        aircraftType: $("#aircraftType").val(),
+        class: $("#class").val(),
+        numEngine: $("#numEngine").val(),
+        tailWheel: $("#tailWheel").val(),
+        complex: $("#complex").val(),
+        highPerf: $("#highPerf").val(),
+        turboFan: $("#turboFan").val(),
+        turboProp: $("#turboProp").val(),
+        rotocraft: $("#rotocraft").val(),
+        poweredLift: $("#poweredLift").val()
+    })
+        .then(result => console.log(result))
+        .catch(err => console.error(err.responseJSON.parent));
 
 }
 // function for getting all flights associated with the logged in user
@@ -239,8 +287,8 @@ function displayFlightTimeTable(flights) {
     for (let i = 0; i < flights.length; i++) {
         // extract the values from flight_time and push them into data array in order to populate the table
         $th = $("<th>")
-        .attr("scope", "row")
-        .text([i + 1]);
+            .attr("scope", "row")
+            .text([i + 1]);
         $tr = $("<tr>")
         for (const values in flights[i]) {
             // push all the flights data into the array, except for the Aircraft object
@@ -255,8 +303,8 @@ function displayFlightTimeTable(flights) {
         };
 
         let $button = $("<button>")
-        .text("Edit")
-        .attr('data-ft-id', flights[i].id);
+            .text("Edit")
+            .attr('data-ft-id', flights[i].id);
         $tr.append($button);
         $tbody.append($th, $tr);
 
