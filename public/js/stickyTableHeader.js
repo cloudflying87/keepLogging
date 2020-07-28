ec82ade104a8240b32d7d17e3d5f76d8a2ce90de
_Pear <63015754+CobyPear@users.noreply.github.com>
2020-07-28 11:49:01 -0500

    commit before pulling master

--- public/js/stickyTableHeader.js
+++ public/js/stickyTableHeader.js
@@ -2,7 +2,7 @@ $(window).scroll(function () {
     const distanceFromTop = $(document).scrollTop();
 
     if (distanceFromTop >= $('.table-header').height() * 2) {
-        $('#sticky').fadeIn(375)
+        $('#sticky').show();
     } else {
         $('#sticky').hide();
     };
