474ace51241bcac76c3ec75b764133aad6199dd9
_Pear <63015754+CobyPear@users.noreply.github.com>
2020-08-06 15:32:21 -0500

    finished service worker. PWA is functional

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,65 +1,77 @@
 const FILES_TO_CACHE = [
-    '/',
-    '/main.html',
-    '/manifest.webmanifest',
-    '/assets/css/style.css',
+    'main.html',
+    'authenticate.html',
+    'manifest.webmanifest',
     '/assets/icons/airplane-icon-72.png',
     '/assets/icons/airplane-icon-96.png',
     '/assets/icons/airplane-icon-256.png',
-    ''
+    '/assets/images/airplane1.jpg',
+    '/assets/images/logo.png',
+    '/assets/images/logoSmall.png',
+    '/assets/images/MenuChevron.png',
+    '/assets/images/panel.jfif',
+    '/assets/images/panel2.jfif',
+    '/assets/images/twoPlanes.jfif',
+    '/assets/js/accordion.js',
+    '/assets/js/authenticate.js',
+    '/assets/js/createLoops.js',
+    '/assets/js/deleteFlight.js',
+    '/assets/js/displayFlights.js',
+    '/assets/js/editFlight.js',
+    '/assets/js/getAircraftTypes.js',
+    '/assets/js/globals.js',
+    '/assets/js/modal.js',
+    '/assets/js/printTotals.js',
+    '/assets/js/stickyTableHeader.js',
+    '/assets/js/writeAircraft.js',
+    '/assets/js/writeFlight.js',
+    '/assets/stylesheets/css/media.css',
+    '/assets/stylesheets/css/modal.css',
+    '/assets/stylesheets/css/signin.css',
+    '/slick/fonts/slick.eot',
+    '/slick/fonts/slick.svg',
+    '/slick/fonts/slick.ttf',
+    '/slick/fonts/slick.woff',
+    '/slick/ajax-loader.gif',
+    '/slick/config.rb',
+    '/slick/slick.css',
+    '/slick/slick.js',
+    '/slick/slick.less',
+    '/slick/slick-theme.css',
+    '/slick/slick-theme.less',
+    '/slick/slick.min.js'
   ];
-  
-  const CACHE_NAME = "static-cache-v1";
+
+  var CACHE_NAME = "my-site-cache-v1";
   const DATA_CACHE_NAME = "data-cache-v1";
-  
-  // install
-  self.addEventListener("install", function(evt) {
-    evt.waitUntil(
-      caches.open(CACHE_NAME).then(cache => {
-        console.log("Your files were pre-cached successfully!");
+
+  self.addEventListener("install", function(event) {
+    // Perform install steps
+    event.waitUntil(
+      caches.open(CACHE_NAME).then(function(cache) {
+        console.log("Opened cache");
         return cache.addAll(FILES_TO_CACHE);
       })
     );
-  
-    self.skipWaiting();
   });
   
-  // activate
-  self.addEventListener("activate", function(evt) {
-    evt.waitUntil(
-      caches.keys().then(keyList => {
-        return Promise.all(
-          keyList.map(key => {
-            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-              console.log("Removing old cache data", key);
-              return caches.delete(key);
-            }
-          })
-        );
-      })
-    );
-  
-    self.clients.claim();
-  });
-  
-  // fetch
-  self.addEventListener("fetch", function(evt) {
-    if (evt.request.url.includes("/api/")) {
-      evt.respondWith(
-        caches.open(DATA_CACHE_NAME)
-        .then(cache => {
-          return fetch(evt.request)
+  self.addEventListener("fetch", function(event) {
+    // cache all get requests to /api routes
+    if (event.request.url.includes("/api/")) {
+      event.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(event.request)
             .then(response => {
               // If the response was good, clone it and store it in the cache.
               if (response.status === 200) {
-                cache.put(evt.request.url, response.clone());
+                cache.put(event.request.url, response.clone());
               }
   
               return response;
             })
             .catch(err => {
               // Network request failed, try to get it from the cache.
-              return cache.match(evt.request);
+              return cache.match(event.request);
             });
         }).catch(err => console.log(err))
       );
@@ -67,12 +79,18 @@ const FILES_TO_CACHE = [
       return;
     }
   
-    evt.respondWith(
-      caches.open(CACHE_NAME).then(cache => {
-        return cache.match(evt.request).then(response => {
-          return response || fetch(evt.request);
+    event.respondWith(
+      fetch(event.request).catch(function() {
+        return caches.match(event.request).then(function(response) {
+          if (response) {
+            return response;
+          } else if (event.request.headers.get("accept").includes("text/html")) {
+            // return the cached home page for all requests for html pages
+            return caches.match("/");
+          }
         });
       })
     );
   });
+  
   
