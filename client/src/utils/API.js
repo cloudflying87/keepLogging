78ff0b71f7cba057981cd7d880139e258d6c6868
_Pear <63015754+CobyPear@users.noreply.github.com>
2020-08-17 11:18:17 -0500

    aircraft routes hooked up to react

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,7 +1,33 @@
 import axios from 'axios';
 
 export default {
-    // aircraft routes
-
-    axios.get
-}
+    // aircraft routes -----------------------------------------------------
+    //get all aircraft
+    getAircraft: function() {
+        return axios.get('api/aircraft/');
+    },
+    // selecting one aircraft
+    getOneAircraft: function(id) {
+        return axios.get('/api/aircraft/'+id);
+    },
+    // create an aiarcraft
+    createAircraft: function() {
+        return axios.post('/api/aircraft/');
+    },
+    // update an aircraft
+    updateAircraft: function(id) {
+        return axios.put('/api/aircraft/update/'+id);
+    },
+    // delete an aircraft
+    deleteAircraft: function(id) {
+        return axios.delete('/api/aircraft/delte/'+id);
+    },
+    // translate aircraft type into id
+    getAircraftId: function(aircraftType) {
+        axios.get('/api/aircraft/userFind/'+aircraftType);
+    },
+    // putting aircraft types in a list for our dropdown menu
+    getAircraftTypes: function() {
+        axios.get('api/aircraftTypes');
+    }
+};
