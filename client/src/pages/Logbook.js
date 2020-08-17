9eb9c0dea658bebd5daeb36b6d19b10fc9526578
_Pear <63015754+CobyPear@users.noreply.github.com>
2020-08-17 18:05:28 -0500

    setup for Logbook page

---
+++ client/src/pages/Logbook.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+
+const Logbook = () => {
+
+    return (
+        <div>
+            <div className='accordion'>
+                {/* here will be the buttons for this page. Maybe i'll make a component for these since there will be one on each page. */}
+                <form>
+                    {/* Here will be the form created by the div. we can hide and show it via state changes. */}
+                </form>
+            </div>
+            <main>
+                {/* The table will live here. Might try to do an actual table first, then will try grid or flexbox. */}
+
+            </main>
+        </div>
+    )
+}
+
+export default Logbook;
