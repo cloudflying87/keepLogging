02a9d2c5b51e534aef16fbd4ef06722fec187edf
_Pear <63015754+CobyPear@users.noreply.github.com>
2020-08-19 14:14:29 -0500

    fixed overlapping styles

--- client/src/components/Modal/index.js
+++ client/src/components/Modal/index.js
@@ -15,33 +15,32 @@ const Modal = ({ handleClick, results }) => {
                     text='X'
                     handleClick={handleClick}
                 />
-                <ul>
-
-                    <li><strong>Date:</strong> {results.date}</li>
+                <ul className='modalUl'>
+                    <li className='modalLi'><strong>Date:</strong> {results.date}</li>
                     {!!results['Aircraft.aircraftType'] &&
-                        <li><strong>Aircraft:</strong> {results['Aircraft.aircraftType']}</li>}
-                    <li><strong>Tail Number:</strong> {results.tailNumber}</li>
-                    <li><strong>Departure Airport:</strong> {results.depAir}</li>
-                    {!!results.enRout && <li><strong>Enroute:</strong> {results.enrRout}</li>}
-                    <li><strong>Arrival Airport:</strong> {results.arrAir}</li>
-                    {!!results.flightNumber && <li><strong>Flight Number</strong> {results.flightNum}</li>}
-                    {!!results.landings && <li><strong>Landings:</strong> {results.landings}</li>}
-                    {!!results.imc && <li><strong>IMC:</strong> {results.imc}</li>}
-                    {!!results.hood && <li><strong>Hood:</strong> {results.hood}</li>}
-                    {!!results.iap && <li><strong>IAP:</strong> {results.iap}</li>}
-                    {!!results.holds && <li><strong>Holds:</strong> {results.holds}</li>}
-                    {!!results.pic && <li><strong>PIC:</strong> {results.pic}</li>}
-                    {!!results.sic && <li><strong>SIC:</strong> {results.sic}</li>}
-                    {!!results.cfi && <li><strong>CFI:</strong> {results.cfi}</li>}
-                    {!!results.dualI && <li><strong>Dual:</strong> {results.cfi}</li>}
-                    {!!results.cxt && <li><strong>Cross Country:</strong> {results.cxt}</li>}
-                    {!!results.solo && <li><strong>Solo:</strong> {results.solo}</li>}
-                    {!!results.total && <li><strong>Total:</strong> {results.total}</li>}
-                    {!!results.dayLdg && <li><strong>Day Landings:</strong> {results.dayLdg}</li>}
-                    {!!results.night && <li><strong>Night:</strong> {results.night}</li>}
-                    {!!results.nightLdg && <li><strong>Night Landings:</strong> {results.nightLdg}</li>}
-                    {!!results.instructor && <li><strong>Instructor:</strong> {results.instructor}</li>}
-                    {!!results.student && <li><strong>Student:</strong> {results.student}</li>}
+                        <li className='modalLi'><strong>Aircraft:</strong> {results['Aircraft.aircraftType']}</li>}
+                    <li className='modalLi'><strong>Tail Number:</strong> {results.tailNumber}</li>
+                    <li className='modalLi'><strong>Departure Airport:</strong> {results.depAir}</li>
+                    {!!results.enRout && <li className='modalLi'><strong>Enroute:</strong> {results.enrRout}</li>}
+                    <li className='modalLi'><strong>Arrival Airport:</strong> {results.arrAir}</li>
+                    {!!results.flightNumber && <li className='modalLi'><strong>Flight Number</strong> {results.flightNum}</li>}
+                    {!!results.landings && <li className='modalLi'><strong>Landings:</strong> {results.landings}</li>}
+                    {!!results.imc && <li className='modalLi'><strong>IMC:</strong> {results.imc}</li>}
+                    {!!results.hood && <li className='modalLi'><strong>Hood:</strong> {results.hood}</li>}
+                    {!!results.iap && <li className='modalLi'><strong>IAP:</strong> {results.iap}</li>}
+                    {!!results.holds && <li className='modalLi'><strong>Holds:</strong> {results.holds}</li>}
+                    {!!results.pic && <li className='modalLi'><strong>PIC:</strong> {results.pic}</li>}
+                    {!!results.sic && <li className='modalLi'><strong>SIC:</strong> {results.sic}</li>}
+                    {!!results.cfi && <li className='modalLi'><strong>CFI:</strong> {results.cfi}</li>}
+                    {!!results.dualI && <li className='modalLi'><strong>Dual:</strong> {results.cfi}</li>}
+                    {!!results.cxt && <li className='modalLi'><strong>Cross Country:</strong> {results.cxt}</li>}
+                    {!!results.solo && <li className='modalLi'><strong>Solo:</strong> {results.solo}</li>}
+                    {!!results.total && <li className='modalLi'><strong>Total:</strong> {results.total}</li>}
+                    {!!results.dayLdg && <li className='modalLi'><strong>Day Landings:</strong> {results.dayLdg}</li>}
+                    {!!results.night && <li className='modalLi'><strong>Night:</strong> {results.night}</li>}
+                    {!!results.nightLdg && <li className='modalLi'><strong>Night Landings:</strong> {results.nightLdg}</li>}
+                    {!!results.instructor && <li className='modalLi'><strong>Instructor:</strong> {results.instructor}</li>}
+                    {!!results.student && <li className='modalLi'><strong>Student:</strong> {results.student}</li>}
 
 
                 </ul>
