7f68c4529cb48eac438e0e0a77113d35bd0630f5
unknown <davidhale87@gmail.com>
2020-08-24 10:27:25 -0500

    adding the / to html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -37,7 +37,7 @@ module.exports = function (app) {
   // });
   */
 
-  app.get((req, res) =>
+  app.get("/", (req, res) =>
     res.sendFile(path.join(__dirname, "../client/build/index.html"))
   );
 
