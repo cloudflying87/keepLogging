e443f799813ced051d4376940576c1042e048dbf
unknown <davidhale87@gmail.com>
2020-08-24 13:38:54 -0500

    Syncing build

---
+++ client/build/precache-manifest.b3a1c13e5cddcffe473f8b5ceedb8eda.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3f3c9575c9c4d1ef41860553286170d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "984ce9c2a6d192ef91de",
+    "url": "/static/css/main.0e4f3eac.chunk.css"
+  },
+  {
+    "revision": "b2036dee011f5e63ad31",
+    "url": "/static/js/2.4348b6bb.chunk.js"
+  },
+  {
+    "revision": "edf358918d0041c12a191abeb1ed6a7a",
+    "url": "/static/js/2.4348b6bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "984ce9c2a6d192ef91de",
+    "url": "/static/js/main.34f394d9.chunk.js"
+  },
+  {
+    "revision": "90a12a883a7a3b2685d3",
+    "url": "/static/js/runtime-main.79067853.js"
+  },
+  {
+    "revision": "80f51e00fa157f185aa46fc698e71270",
+    "url": "/static/media/logoSmall.80f51e00.png"
+  }
+]);
