a2b9e22d26d9d1d514c9b5c3e3727d4f52dd40e7
_Pear <63015754+CobyPear@users.noreply.github.com>
2020-09-23 14:23:25 -0500

    added path to importCSV

--- importCSV.js
+++ importCSV.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const mysql = require('mysql2');
 const csv = require('fast-csv');
+const path = require("path");
 require('dotenv').config();
 
 let host = process.env.HOST_LOCAL
@@ -12,35 +13,42 @@ let connection;
 
 
 
-var importFile = ['./db/Users.csv','./db/airportone.csv','./db/airporttwo.csv','./db/aircraftModels.csv','./db/aircraft.csv','./db/sampledata.csv']
+var importFile = [
+    path.join(__dirname, './db/Users.csv'),
+    path.join(__dirname, './db/airportone.csv'),
+    path.join(__dirname, './db/airporttwo.csv'),
+    path.join(__dirname, './db/aircraftModels.csv'),
+    path.join(__dirname, './db/aircraft.csv'),
+    path.join(__dirname, './db/sampledata.csv')
+]
 for (let i = 0; i < importFile.length; i++) {
     let stream = fs.createReadStream(importFile[i]);
     let myData = [];
-    
+
     let csvStream = csv
         .parse()
         .on("data", function (data) {
             myData.push(data);
-            
+
         })
         .on("end", function () {
             myData.shift();
 
-        if (process.env.JAWSDB_URL) {
-            connection = mysql.createConnection(process.env.JAWSDB_URL);
-        } else {
-            connection = mysql.createConnection({
-                host: host,
-                port: port,
-                user: user,
-                password: password,
-                database: database,
-            });
-        };
+            if (process.env.JAWSDB_URL) {
+                connection = mysql.createConnection(process.env.JAWSDB_URL);
+            } else {
+                connection = mysql.createConnection({
+                    host: host,
+                    port: port,
+                    user: user,
+                    password: password,
+                    database: database,
+                });
+            };
             connection.connect((error) => {
-                
+
                 if (error) {
-                    
+
                     console.error(error);
                 } else if (importFile[i] == './db/sampledata.csv') {
                     let query = 'INSERT INTO FlightTimes (id,UserId,date,AircraftId,route,flightNum,depTime,arrTime,landings,imc,hood,iap,holds,pic,sic,cfi,dualI,cxt,solo,total,dayLdg,night,nightLdg,comments,instructor,student,createdAt,updatedAt) VALUES ?';
@@ -53,13 +61,13 @@ for (let i = 0; i < importFile.length; i++) {
                     connection.query(query, [myData], (error, response) => {
                         console.log(error || response);
                     })
-                } else if (importFile[i] == './db/airportone.csv') {  
+                } else if (importFile[i] == './db/airportone.csv') {
                     console.log("airports")
                     let query = 'INSERT INTO Airports (id,icao,iata,name,city,state,country,elevation,lat,lon,tz,createdAt,updatedAt) VALUES ?'
                     connection.query(query, [myData], (error, response) => {
                         console.log(error || response);
                     })
-                } else if (importFile[i] == './db/airporttwo.csv') {  
+                } else if (importFile[i] == './db/airporttwo.csv') {
                     console.log("airports")
                     let query = 'INSERT INTO Airports (id,icao,iata,name,city,state,country,elevation,lat,lon,tz,createdAt,updatedAt) VALUES ?'
                     connection.query(query, [myData], (error, response) => {
@@ -71,19 +79,19 @@ for (let i = 0; i < importFile.length; i++) {
                     connection.query(query, [myData], (error, response) => {
                         console.log(error || response);
                     });
-                
-                } else if (importFile[i] == './db/aircraft.csv') {  
+
+                } else if (importFile[i] == './db/aircraft.csv') {
                     console.log("aircraft")
                     let query = 'INSERT INTO Aircraft (id,tailNumber,AircraftModelId,avionics,createdAt,updatedAt) VALUES ?'
                     connection.query(query, [myData], (error, response) => {
                         console.log(error || response);
                     })
                 } else {
-                    console.log("Test" ,myData.length)
+                    console.log("Test", myData.length)
                 }
             });
         });
     stream.pipe(csvStream);
-    
+
 }
 
