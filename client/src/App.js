1ebd87c2350e5db79b3dadb7523de68724611b5b
_Pear <63015754+CobyPear@users.noreply.github.com>
2021-02-16 19:12:21 -0600

    removed userContext as it wasn't working and wont be needed when redux is implemented

--- client/src/App.js
+++ client/src/App.js
@@ -8,25 +8,11 @@ import Airports from './pages/Airports';
 import MyProfile from './pages/MyProfile';
 import Redirect from './pages/Validate';
 import './style.css'
-import UserContext from './utils/UserContext'
 
 
 
 function App() {
-
-  const [user, setUser] = useState({
-    userId: '',
-    updateUser: function (userId) {
-      setUser({
-        ...user,
-        userId
-      })
-    }
-  })
-
-  
   return (
-    <UserContext.Provider value={user}>
       <Router>
         <Switch>
           <Route exact path='/' component={Signup} />
@@ -38,7 +24,6 @@ function App() {
           <Route exact path='/redirect/:key/:studentID/:studentEmail' component={Redirect} />
         </Switch>
       </Router>
-    </UserContext.Provider>
   );
 }
 
--- client/src/utils/UserContext.js
+++
@@ -1,7 +0,0 @@
-import React, { createContext } from 'react';
-
-const UserContext = createContext({
-    userId: JSON.parse(localStorage.getItem('userId'))
-});
-
-export default UserContext;
